ca3a86b866b5679a1a1b66582f75d4ad6f1cd856
Sami Lappalainen <lappalainen.sami@outlook.com>
2025-01-18 18:03:54 +0100

    product descriptions

--- js/products.js
+++ js/products.js
@@ -25,7 +25,7 @@ class Product {
 }
 
 let products = [];
-products.push(new Product("spork", "SPORK, din bästa vän i naturen!", 12.90, "img/spork-color.jpg", ["Blå", "Grön", "Rosa", "Gul"]))
-products.push(new Product("fork", "FORK (it's just a fork)", 7.90, "img/fork.jpg", ["Large", "Medium", "Small"]))
-products.push(new Product("knife", "KNIFE, for slicin'", 6.90, "img/knife.jpg", ["Sharp", "Dull", "Rusty"]))
-products.push(new Product("spoon", "SPOON. Eat soup.", 7.90, "img/spoon.jpg", ["Tea-sized", "Normal"]))
+products.push(new Product("spork", "A versatile 2-in-1 utensil that combines the functionality of a spoon and fork. Crafted from durable materials, it’s perfect for camping, picnics, travel, or everyday meals. Lightweight and easy to clean, the spork saves space and simplifies your dining experience.", 12.90, "img/spork-color.jpg", ["Blå", "Grön", "Rosa", "Gul"]))
+products.push(new Product("fork", "A high-quality fork designed for everyday use, featuring a comfortable grip and polished tines for smooth handling. Whether you’re enjoying a hearty meal or a delicate dish, this fork is a reliable choice for your tableware collection.", 7.90, "img/fork.jpg", ["Large", "Medium", "Small"]))
+products.push(new Product("knife", "A sharp and sturdy knife designed for precision cutting, whether you're slicing through meats, vegetables, or bread. With an ergonomic handle for a secure grip, this knife combines practicality and durability, making it an indispensable kitchen tool.", 6.90, "img/knife.jpg", ["Sharp", "Dull", "Rusty"]))
+products.push(new Product("spoon", "This durable and elegant spoon is perfect for everything from soups and stews to desserts and cereal. Its smooth edges and ergonomic design make it both practical and stylish, an essential addition to any kitchen or dining set.", 7.90, "img/spoon.jpg", ["Tea-sized", "Normal"]))
